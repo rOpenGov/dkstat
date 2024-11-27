1a5d986420b7cbaca942d449c018d9dbd925fe40
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-11-27 22:10:39 +0100

    Update test to new table
    Closes #35

--- tests/testthat/test-dst_value_limit.R
+++ tests/testthat/test-dst_value_limit.R
@@ -4,12 +4,23 @@
 context("dst_value_limit")
 
 test_that("dst_value_limit returns the correct value.", {
-  
-  expect_equal(dst_value_limit(query = list(CIVILSTAND = "*", 
-                                            STATSB = "*", 
-                                            HERKOMST = c("Personer med dansk oprindelse", "Efterkommere")), 
-                               dst_meta = dst_meta("folk1", lang = "da")), 3318)
-  
-  
+
+  expect_equal(dst_value_limit(query = list(CIVILSTAND = "*",
+                                            ALDER = "*",
+                                            OMRÅDE = c(
+                                              "Hele Landet",
+                                              "København",
+                                              "Dragør",
+                                              "Albertslund",
+                                              "Christiansø",
+                                              "Køge",
+                                              "Glostrup",
+                                              "Faaborg-Midtfyn",
+                                              "Holstebro",
+                                              "Læsø"
+                                            )),
+                               dst_meta = dst_meta("folk1a", lang = "da")), 6350)
+
+
 })
 
